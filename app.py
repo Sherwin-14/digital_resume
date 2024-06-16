c41d030cb5808299da6250058fb7f8ea6baeeae6
Sherwin Varghese <141290943+Sherwin-14@users.noreply.github.com>
2024-06-16 17:28:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ PERSONAL_PROJECTS={
 
     "🏆 English Premier League Analysis - An Analysis of EPL Season 20/21 " : "https://github.com/Sherwin-14/English_Premier_Analysis",
     "🏆 Polish Property Market Overview - Understanding The Market Through Otodom " : "https://github.com/Sherwin-14/Polish-Property-Market-Overview",
-    "🏆 Atliq Mart Supply Chain Optimization - : Journey to Supply Chain Excellence":"https://github.com/Sherwin-14/PixieDust/tree/master",
-    "🏆 Metropolitan Motion: The Analytic Route of NY’s Yellow Cabs" :"https://github.com/Sherwin-14/Hospitality_Domain ",
+    "🏆 Atliq Mart Supply Chain Optimization - Journey to Supply Chain Excellence":"https://github.com/Sherwin-14/PixieDust/tree/master",
+    "🏆 Metropolitan Motion - The Analytic Route of NY’s Yellow Cabs" :"https://github.com/Sherwin-14/Hospitality_Domain ",
     "🏆 Atliq Telecom User Analysis -  Mapping User Trends at Atliq Telecom" :"https://github.com/Sherwin-14/BeatBoard ",
     "🏆 Revenue Insights Hospitality Domain - Dashboard on Hospitality Domain" :"https://github.com/Sherwin-14/Hospitality_Domain ",
     
